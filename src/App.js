287d7eb606bf6aa7eae672854637a09692dc7784
Venkat <146267552+Venkatpublic@users.noreply.github.com>
2024-04-29 23:09:52 +0530

    Revert "Immer objects"

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@
 import './App.css';
 import PlayList from './screens/playlist';
 import Artists from "./screens/artists"
-import { useEffect } from 'react';
 function App() {
-
   return (
     <div style={{display:'flex',flexDirection:'row'}} className="App" >
     <PlayList ></PlayList>
