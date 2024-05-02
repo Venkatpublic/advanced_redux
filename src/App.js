717c1784b51c168cf1a02976cc53a51545fdc762
Venkatasalam <venkatasalam0412@gmail.com>
2024-05-02 23:40:29 +0530

    playing with immer and react hooks for deep updates

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 import PlayList from './screens/playlist';
 import Artists from "./screens/artists"
+import Profile from './screens/profile';
 import { useEffect } from 'react';
 function App() {
 
   return (
     <div style={{display:'flex',flexDirection:'row'}} className="App" >
-    <PlayList ></PlayList>
-    <Artists></Artists>
+      <Profile></Profile>
+    {/* <PlayList ></PlayList>
+    <Artists></Artists> */}
     </div>
   );
 }
