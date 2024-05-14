ae4b8268d09341a80c250010557d2672d327b14f
Venkatasalam <venkatasalam0412@gmail.com>
2024-05-14 22:20:52 +0530

    creatSlice API implemented

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 
 import './App.css';
-import PlayList from './screens/playlist';
-import Artists from "./screens/artists"
+import Home from './features/home/Home';
 
-import Profile from './screens/profile';
-import Patches from './screens/patches';
-import { useEffect } from 'react';
-import {enablePatches} from "immer"
+
 
 function App() {
 
   return (
-<div></div>
+<div>
+  <Home></Home>
+</div>
   );
 }
 
